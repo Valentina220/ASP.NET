d38ffb83ac08829ee26c4eb3143f7f13ec3350f2
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2021-04-29 13:30:11 +0300

    Add files via upload

---
+++ lab4/CodeFirst/WingtipToys.Data/SqlCategoriesRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingtipToys.Data
+{
+    public class SqlCategoryRepository : ICategoryRepository
+    {
+        public Category Create(Category category)
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                var added = ctx.Categories.Add(category);
+                ctx.SaveChanges();
+                return added;
+            }
+
+        }
+
+        public bool Delete(int categoryId)
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                var existing = ctx.Categories.SingleOrDefault(x => x.CategoryID == categoryId);
+                if (existing == null)
+                {
+                    return false;
+                }
+                ctx.Categories.Remove(existing);
+                ctx.SaveChanges();
+                return true;
+            }
+
+        }
+
+        public Category Get(string categoryName)
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Categories.SingleOrDefault(x => x.CategoryName.Equals(categoryName, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+        }
+
+        public Category Get(int categoryId)
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Categories.SingleOrDefault(x => x.CategoryID == categoryId);
+            }
+
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Categories.ToList();
+            }
+
+        }
+
+        public Category Update(Category category)
+        {
+            //throw new NotImplementedException();
+            using (var ctx = new WingtipToysEntities())
+            {
+                var existing = ctx.Categories.SingleOrDefault(x => x.CategoryID == category.CategoryID);
+                if (existing == null)
+                {
+                    return null;
+                }
+                existing.Description = category.Description;
+                existing.CategoryName = category.CategoryName;
+                ctx.SaveChanges();
+                return existing;
+            }
+
+        }
+    }
+
+}
---
+++ lab4/CodeFirst/WingtipToys.Data/SqlProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingtipToys.Data
+{
+    public class SqlProductRepository : IProductRepository
+    {
+
+        public Product Create(Product product)
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                var added = ctx.Products.Add(product);
+                ctx.SaveChanges();
+                return added;
+            }
+        }
+
+        public Product Update(Product product)
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                var existing = ctx.Products.SingleOrDefault(x => x.ProductID == product.ProductID);
+                if (existing == null)
+                {
+                    return null;
+                }
+                existing.CategoryID = product.CategoryID;
+                existing.Description = product.Description;
+                existing.ImagePath = product.ImagePath;
+                existing.ProductName = product.ProductName;
+                existing.UnitPrice = product.UnitPrice;
+                ctx.SaveChanges();
+                return existing;
+            }
+        }
+
+        public bool Delete(int productId)
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                var existing = ctx.Products.SingleOrDefault(x => x.ProductID == productId);
+                if (existing == null)
+                {
+                    return false;
+                }
+                ctx.Products.Remove(existing);
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+
+        public Product Get(int productId)
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Products.SingleOrDefault(x => x.ProductID == productId);
+            }
+        }
+
+        public IEnumerable<Product> GetByCategoryId(int categoryId)
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Products.Where(x => x.CategoryID == categoryId).ToList();
+            }
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            using (var ctx = new WingtipToysEntities())
+            {
+                return ctx.Products.ToList();
+            }
+        }
+
+    }
+}
