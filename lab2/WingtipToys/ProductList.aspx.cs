f073ce609d06049bee8f9cf7f6adc2e8eacdd75f
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2021-04-29 13:08:32 +0300

    Add files via upload

---
+++ lab2/WingtipToys/ProductList.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Business;
+using WingtipToys.Data;
+using WingtipToys.Data.Models;
+
+namespace WingtipToys
+{
+    public partial class ProductList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static readonly IStoreService _service = new StoreService(new InMemoryProductRepository(), new InMemoryCategoryRepository());
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId, [RouteData] string categoryName)
+        {
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                return _service.GetProductByCategoryId(categoryId.Value).AsQueryable();
+            }
+
+            if (!string.IsNullOrEmpty(categoryName))
+            {
+                return _service.GetProductByCategoryName(categoryName).AsQueryable();
+            }
+            return _service.GetAllProducts().AsQueryable();
+        }
+        
+    }
+}
